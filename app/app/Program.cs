eba9d8548c0d6767253c6e16f9a71dc0059b747a
ContaEscola <92809778+ContaEscola@users.noreply.github.com>
2022-06-03 11:31:27 +0100

    Dph 71 criar formulario individual de restaurante (#13)
    * DPH-32 /Parte do formulário feito
    
    Falta trocar as fonts
    
    * DPH-32 /Formulário feito por complete
    
    Formulário já tem as fontes corretas.
    
    * Fixed rebase with DPH-32
    
    * DPH-30 /Atualizei formulário
    
    * DPH-30 /Formulário completo
    
    Formulário com as fontes corretas!
    
    * Fixed rebase with DPH-30
    
    * Fix do rebase com DPH-28
    
    * Fix do rebase com DPH-30
    
    * DPH-28 /Errors fixed
    
    * DPH-28 /Formulário feito
    
    Formulário com as fontes corretas!
    Atualizei o método LoadFontsForControls e LoadFontsForToolStripItems no FontLoader
    e todas os outros formulários que usavam este método.
    
    * Fixed rebase 1 from DPH-28
    
    * Fix rebase 2 from DPH-28
    
    * Fix rebase 3 from DPH-28
    
    * Fix rebase 4 from DPH-28
    
    * DPH-29 /Formulário completo
    
    Formulário já tem as fontes corretas!
    Mudei o backColor de todos os butões para 255,255,255 (White).
    
    * DPH-71 /Formulário feito
    
    Criei o formulário já com as fontes corretas
    
    Co-authored-by: TheSmiley007 <pedronorberto368@gmail.com>

--- app/app/Program.cs
+++ app/app/Program.cs
@@ -16,8 +16,7 @@ namespace app
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
-
+            Application.Run(new Restaurant());
         }
     }
 }
---
+++ app/app/mvc/views/Restaurant.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app
+{
+    public partial class Restaurant : Form
+    {
+        private FontLoader _fontLoader;
+
+
+        public Restaurant()
+        {
+            InitializeComponent();
+
+            _fontLoader = new FontLoader();
+        }
+
+
+        //Irá dar load das fontes em todos os controlos do form
+        private void Restaurant_Load(object sender, EventArgs e)
+        {
+            Dictionary<Control, string> fontsForControls = new Dictionary<Control, string>();
+
+            fontsForControls.Add(Lbl_RestauranteName, FontLoader.RobotoSlabBold);
+
+            fontsForControls.Add(Lbl_CodPostal, FontLoader.RobotoSlabExtraLight);
+            fontsForControls.Add(Lbl_RestaurantCodPostal, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_City, FontLoader.RobotoSlabExtraLight);
+            fontsForControls.Add(Lbl_RestaurantCity, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_Country, FontLoader.RobotoSlabExtraLight);
+            fontsForControls.Add(Lbl_RestaurantCountry, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_Street, FontLoader.RobotoSlabExtraLight);
+            fontsForControls.Add(Lbl_RestaurantStreet, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_AddWorker, FontLoader.RobotoSlabLight);
+
+            fontsForControls.Add(Lbl_NewWorkerName, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_NewWorkerName, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_NewWorkerPhone, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(MaskedTxtBox_NewWorkerPhone, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_NewWorkerSalary, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(MaskedTxtBox_NewWorkerSalary, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_NewWorkerPosition, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_NewWorkerPosition, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_NewWorkerStreet, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_NewWorkerStreet, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_NewWorkerCity, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_NewWorkerCity, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_NewWorkerPostalCode, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(MaskedTxtBox_NewWorkerPostalCode, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Btn_AddWorker, FontLoader.RobotoSlabRegular);
+
+
+            fontsForControls.Add(Lbl_EditWorker, FontLoader.RobotoSlabLight);
+
+            fontsForControls.Add(Lbl_WorkerName, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_WorkerName, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_WorkerPhone, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(MaskedTxtBox_WorkerPhone, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_WorkerSalary, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(MaskedTxtBox_WorkerSalary, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_WorkerPosition, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_WorkerPosition, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_WorkerStreet, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_WorkerStreet, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_WorkerCity, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(TxtBox_WorkerCity, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_WorkerPostalCode, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(MaskedTxtBox_WorkerPostalCode, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Lbl_WorkerState, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(ComboBox_WorkerState, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Btn_SaveChangesOnWorker, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Btn_RemoveWorker, FontLoader.RobotoSlabRegular);
+
+            fontsForControls.Add(Btn_ManageMenu, FontLoader.RobotoSlabRegular);
+            fontsForControls.Add(Btn_ManageOrders, FontLoader.RobotoSlabRegular);
+
+            FontFamily fontForDataGrid = _fontLoader.GetFont(FontLoader.RobotoSlabRegular);
+            DataGridView_Workers.ColumnHeadersDefaultCellStyle.Font = new Font(fontForDataGrid, DataGridView_Workers.ColumnHeadersDefaultCellStyle.Font.Size);
+            DataGridView_Workers.DefaultCellStyle.Font = new Font(fontForDataGrid, DataGridView_Workers.DefaultCellStyle.Font.Size);
+
+            _fontLoader.LoadFontToControls(fontsForControls);
+
+            Lbl_AddWorker.BringToFront();
+            Lbl_EditWorker.BringToFront();
+        }
+    }
+}

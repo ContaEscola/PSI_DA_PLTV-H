db8833a4f840258a4e30ab9b79fffe672e0dbcce
ContaEscola <2211904@my.ipleiria.pt>
2022-06-22 14:26:16 +0100

    DPH-102 /Funcionalidade feita

--- app/app/mvc/controllers/CRUD.cs
+++ app/app/mvc/controllers/CRUD.cs
@@ -19,5 +19,24 @@ namespace app
             SingleTown.AppDB.MetodoPagamentoSet.Add(paymentMethodToAdd);
             SingleTown.AppDB.SaveChanges();
         }
+
+        public static void EditPaymentMethod(MetodoPagamento updatedPaymentMethod)
+        {
+            var allPaymentMethods = SingleTown.AppDB.MetodoPagamentoSet;
+
+            MetodoPagamento paymentMethodToUpdate = (MetodoPagamento)allPaymentMethods.Where(p => p.Metodo == SingleTown.SelectedPaymentMethod.Metodo).FirstOrDefault();
+
+
+            MetodoPagamento checkForExistentNames = (MetodoPagamento)allPaymentMethods.Where(p => p.Metodo == updatedPaymentMethod.Metodo && p.Id != paymentMethodToUpdate.Id).FirstOrDefault();
+
+            if (checkForExistentNames != null)
+                throw new Exception("Já existe um método de pagamento com esta descrição!");
+
+            paymentMethodToUpdate.Metodo = updatedPaymentMethod.Metodo;
+            paymentMethodToUpdate.Ativo = updatedPaymentMethod.Ativo;
+
+            SingleTown.AppDB.SaveChanges();
+
+        }
     }
 }
